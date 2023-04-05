b2074bd154ecc441a6042efa96d9131207b8bbc3
GtDavide01 <davidefiorini01@gmail.com>
2023-04-05 15:02:35 +0200

    Add: Component e Routing in header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './componenti/about/about.component';
+import { ContactComponent } from './componenti/contact/contact.component';
+import { HomeComponent } from './componenti/home/home.component';
+import { HeaderComponent } from './componenti/header/header.component';
+import { MainComponent } from './componenti/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
